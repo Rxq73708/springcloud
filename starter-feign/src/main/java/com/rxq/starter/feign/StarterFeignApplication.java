8790a7fc0829c734aba8df3c05d02fb8575b9bcd
Rxq73708 <2856673708@qq.com>
2019-02-14 09:46:33 +0800

    starter-feign第一次提交

---
+++ starter-feign/src/main/java/com/rxq/starter/feign/StarterFeignApplication.java
@@ -0,0 +1,14 @@
+package com.rxq.starter.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarterFeignApplication.class, args);
+    }
+
+}
+
